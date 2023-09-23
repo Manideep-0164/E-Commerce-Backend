f34b33a1f417ceeec9eee50fe74a8b82319f1a5f
Manideep-0164 <peddaboinimanideep03@gmail.com>
2023-09-23 14:40:56 +0530

    Added OrderRouter API endpoints for retrieving total orders and a single order

--- routes/order.router.js
+++ routes/order.router.js
@@ -45,6 +45,66 @@ orderRouter.post("/place-order", async (req, res) => {
   }
 });
 
+orderRouter.get("/get-orders", async (req, res) => {
+  try {
+    const userId = req.user;
+
+    const orders = await OrderModel.find({ user: userId })
+      .select(
+        "_id user totalQuantity totalPrice address status createdAt items"
+      )
+      .populate({
+        path: "items.product",
+        select: "title brand price imageUrl",
+      });
+
+    if (orders.length === 0)
+      return res.status(404).json({ message: "Orders not found." });
+
+    if (orders[0].items.length === 0)
+      return res.status(204).json({ message: "Order is empty" });
+
+    res.status(200).json(orders);
+  } catch (error) {
+    console.log("Error fetching cart:", error);
+    res
+      .status(500)
+      .json({ error: "Something went wrong. Please try again later." });
+  }
+});
+
+orderRouter.get("/get-order/:orderId", async (req, res) => {
+  try {
+    const userId = req.user;
+    const { orderId } = req.params;
+
+    const order = await OrderModel.find({ _id: orderId, user: userId })
+      .select(
+        "_id user totalQuantity totalPrice address status createdAt items"
+      )
+      .populate({
+        path: "items.product",
+        select: "title brand price imageUrl",
+      });
+
+    if (order.length === 0)
+      return res.status(404).json({ message: "Order not found." });
+
+    if (order[0].items.length === 0)
+      return res.status(204).json({ message: "Order is empty" });
+
+    res.status(200).json(order);
+  } catch (error) {
+    if (error.name === "CastError") {
+      return res.status(400).json({ error: "Invalid product ID." });
+    }
+    console.log("Error fetching cart:", error);
+    res
+      .status(500)
+      .json({ error: "Something went wrong. Please try again later." });
+  }
+});
+
 module.exports = {
   orderRouter,
 };
